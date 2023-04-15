79ae5150a98c832c9059d1333a393d59d318b63d
Fred Eugine Adomako <fredadomako2003@gmail.com>
2023-04-15 23:27:58 +0200

    .

---
+++ es-62/exercise-3.js
@@ -0,0 +1,13 @@
+function isAdult() {
+  return age >= 18;
+}
+
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+const {age} = person
+
+console.log(isAdult(person));
