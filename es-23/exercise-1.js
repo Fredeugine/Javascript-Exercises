734f4efe1a945d064612698613b51b164fc29ea0
Fred Eugine Adomako <vimportantemails@gmail.com>
2023-04-25 23:34:46 +0200

    .

--- es-23/exercise-1.js
+++ es-23/exercise-1.js
@@ -4,13 +4,8 @@ const person = {
   age: 25
 }
 const newFormat = Object.keys(person);
-const newFormat2 =  Object.values(person);
-
-for (let i = 0; i < newFormat.length; i++) {
-  const key = newFormat[i];
-  const val = newFormat2[i]
-
-  console.log(`${key}: ${val}`);
-}
+newFormat.forEach(function (value){
+  console.log(value + '/' + person[value])
+})
 
 // Print values of person using Object.keys
