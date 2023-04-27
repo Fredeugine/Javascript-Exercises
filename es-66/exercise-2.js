888f767dd5285ab28001a44b4622ceaadd862c48
Fred Eugine Adomako <vimportantemails@gmail.com>
2023-04-27 14:47:12 +0200

    .

--- es-66/exercise-2.js
+++ es-66/exercise-2.js
@@ -4,14 +4,7 @@ const person = {
   lastName: 'Rossi',
   age: 25
 };
-// set ...test for all person properties
-const {...test} = person
-const personInfo = {
-  firstName: person.firstName,
-  lastName: person.lastName,
-  age: person.age
-};
-// set ...rest for all personInfo properties
-const {...rest} = personInfo
-// use test['id'] as reference the person object
-console.log(test['id'], rest);
+
+const {id,...rest} = person
+
+console.log(id,rest);
