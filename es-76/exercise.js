15fb331beab1a8e8623f946660b1c2ca1720761a
Fred Eugine Adomako <vimportantemails@gmail.com>
2023-04-28 13:14:30 +0200

    .

--- es-76/exercise.js
+++ es-76/exercise.js
@@ -1,22 +1,17 @@
-const number = 15;
 
-function checkIfGreaterThan15(number) {
+function checkIfGreaterThan15(num) {
     return new Promise(function (resolve, reject) {
-        if (number > 10) {
-            resolve(number)
-        } else reject(number)
+        if (num > 10) {
+            resolve(num)
+        } else reject(num)
     });
 }
 
 
-function results(number) {
-    checkIfGreaterThan15(number).then(greaterThan10).catch(lessThan10)
-}
-function greaterThan10(data) {
-    console.log(data + ' is greater than 10')
-}
 
-function lessThan10(data) {
+const number = 15;
+checkIfGreaterThan15(number).then(function (numbers){
+    console.log(numbers + ' is greater than 10')
+}).catch(function (data){
     console.error(data + ' is less than or equal to 10')
-}
-results(number)
+})
