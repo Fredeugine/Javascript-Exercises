694f5881a515a554778545ece92f398f4bb49a1a
fredeugine <fredadomako2003@gmail.com>
2023-05-02 19:36:44 +0200

    done

--- ls79/exercise.js
+++ ls79/exercise.js
@@ -8,9 +8,3 @@ function storeUser(obj){
 	localStorage.setItem('user',obj)
 }
 storeUser(JSON.stringify(user))
-
-function retrieveUser(key){
-	data = localStorage.getItem(key)
-console.log(JSON.parse(data))
-}
-retrieveUser('user')
---
+++ ls80/exercise.js
@@ -0,0 +1,16 @@
+const user = {
+	id: 1,
+	name: "John",
+	age: 25,
+};
+
+function storeUser(obj) {
+	localStorage.setItem('user', obj)
+}
+storeUser(JSON.stringify(user))
+
+function retrieveUser(key) {
+	data = localStorage.getItem(key)
+	console.log(JSON.parse(data))
+}
+retrieveUser('user')
